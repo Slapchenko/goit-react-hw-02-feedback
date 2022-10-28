7313104b7fd4c27c09522b8646fa96d8485bbdc4
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-10-28 23:25:04 +0300

    add component styles

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -1,15 +1,15 @@
 import styled from '@emotion/styled';
 
 export const Button = styled.button`
-  display: inline-block;
+  width: 100%;
   padding: 0.35em 1.2em;
   background-color: inherit;
   border: 0.1em solid #ffffff;
-  margin: 0 0.3em 0.3em 0;
+  margin-bottom: 15px;
   border-radius: 0.12em;
   box-sizing: border-box;
   text-decoration: none;
-  font-family: 'Roboto', sans-serif;
+  font-size: 15px;
   font-weight: 300;
   color: #ffffff;
   text-align: center;
--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -2,4 +2,8 @@ import styled from '@emotion/styled';
 
 export const PageSection = styled.section``;
 
-export const Title = styled.h2``;
+export const Title = styled.h2`
+  text-align: center;
+  margin-bottom: 15px;
+  font-size: 32px;
+`;
