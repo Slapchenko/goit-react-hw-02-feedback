34e4e7dec1cd17059d347b7b180dad0ee9d78c5a
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-10-28 14:54:34 +0000

    Update 3 files

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -1,3 +1,22 @@
 import styled from '@emotion/styled';
 
-export const Button = styled.button``;
+export const Button = styled.button`
+  display: inline-block;
+  padding: 0.35em 1.2em;
+  background-color: inherit;
+  border: 0.1em solid #ffffff;
+  margin: 0 0.3em 0.3em 0;
+  border-radius: 0.12em;
+  box-sizing: border-box;
+  text-decoration: none;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 300;
+  color: #ffffff;
+  text-align: center;
+  transition: all 0.2s;
+
+  :hover {
+    color: #000000;
+    background-color: #ffffff;
+  }
+`;
